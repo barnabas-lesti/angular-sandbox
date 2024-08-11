8a0e1d7b441b1990c3989db3ece5033c7cd6be08
Barnabas Lesti <barnabas.lesti@gmail.com>
2024-08-11 21:29:35 +0200

    chore(*): move ssr server file to src

--- src/ssr.server.ts
+++ src/ssr.server.ts
@@ -4,7 +4,7 @@ import express from "express";
 import { dirname, join, resolve } from "node:path";
 import { fileURLToPath } from "node:url";
 
-import bootstrap from "./src/main.server";
+import bootstrap from "./main.server";
 
 // The Express app is exported so that it can be used by serverless Functions.
 export function app(): express.Express {
